efe28d02491146b07b9297a2f41bd16b4eceffff
386592336@qq.com <386592336@qq.com>
2019-06-21 11:37:28 +0800

    Swagger配置修改

--- src/main/java/com/netease/NcmApplication.java
+++ src/main/java/com/netease/NcmApplication.java
@@ -2,10 +2,8 @@ package com.netease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class NcmApplication {
     public static void main(String[] args) {
         SpringApplication.run(NcmApplication.class);
--- src/main/java/com/netease/config/Swagger2.java
+++ src/main/java/com/netease/config/Swagger2.java
@@ -1,21 +1,28 @@
 package com.netease.config;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class Swagger2 {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
+                //加了ApiOperation注解的类，生成接口文档
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                //包下的类，生成接口文档（自动映射）
                 .apis(RequestHandlerSelectors.basePackage("com.netease.controller"))
                 .paths(PathSelectors.any())
                 .build();
@@ -23,7 +30,10 @@ public class Swagger2 {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .title("网易云音乐 API")
+                .description("网易云音乐 Java API service")
+                .termsOfServiceUrl("https://music.163.com/")
+                .contact(new Contact("童重远", "https://aszt.github.io/", "tongchongyuan@163.com"))
                 .version("1.0")
                 .build();
     }
